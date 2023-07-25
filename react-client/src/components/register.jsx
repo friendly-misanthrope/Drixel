85f415384b3c10d8bf3bafa09952c4983305366d
NickBarkerDev <nick.barker.devops@gmail.com>
2023-07-24 22:12:42 -0700

    added user state and change handler

--- react-client/src/components/register.jsx
+++ react-client/src/components/register.jsx
@@ -3,6 +3,20 @@ import { useNavigate, Link } from 'react-router-dom'
 import axios from 'axios'
 
 const Register = (props) => {
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const navigate = useNavigate()
+
+  const changeHandler = (e) => {
+    setUser(prevState => {return {...prevState, [e.target.name]: e.target.value}})
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
